0aaeff24a95fa13fd1527e6d8980e8270183200f
Adrián Bailador Panero <adrianbailador@hotmail.com>
2023-03-11 11:22:53 +0000

    Implement ChatService

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,8 @@
+using Api.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,17 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ChatService>();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
---
+++ Api/Services/ChatService.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Api.Services
+{
+    public class ChatService
+    {
+        //Key, Value, for example: { {"adri", ""2532432!@ahhhash}, {"david", ""ejejej!@ahhhash} }
+        private static readonly Dictionary<string, string> Users = new Dictionary<string, string>();
+
+        public bool AddUserToList(string userToAdd)
+        {
+            lock (Users)
+            {
+                foreach (var user in Users)
+                {
+                    //jOhn john
+                    if(user.Key.ToLower() == userToAdd.ToLower())
+                    {
+                        return false;
+                    }
+                }
+                Users.Add(userToAdd, null);
+                return true;
+            }
+        }
+
+        public void AddUserConnectionId(string user, string connectionId)
+        {
+            lock (Users)
+            {
+                if(Users.ContainsKey(user))
+                {
+                    Users[user] = connectionId;
+                }
+            }
+        }
+
+        public string GetUserByConnectionId(string connectionId) 
+        {
+            lock (Users)
+            {
+                return Users.Where(x=> x.Value == connectionId).Select(x=> x.Key).FirstOrDefault();
+            }
+        }
+
+        public string GetUserByUser(string user)
+        {
+            lock (Users)
+            {
+                return Users.Where(x => x.Value == user).Select(x => x.Value).FirstOrDefault();
+            }
+        }
+        public void RemoveUserFromList(string user)
+        {
+            lock(Users)
+            {
+                if (Users.ContainsKey(user))
+                {
+                    Users.Remove(user);
+                }
+            }
+        }
+
+        public string[] GetOnlineUsers()
+        {
+            lock (Users)
+            {
+                return Users.OrderBy(x => x.Key).Select(x=> x.Key).ToArray();
+            }
+        }
+    }
+}

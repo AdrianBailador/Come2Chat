9822eb8a80d11ee4c2d5de8d2615b1187dee2699
Adrián Bailador Panero <adrianbailador@hotmail.com>
2023-03-23 18:31:33 +0000

    online users

--- ClientApp/src/app/services/chat.service.ts
+++ ClientApp/src/app/services/chat.service.ts
@@ -2,16 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
+import { HubConnection } from '@microsoft/signalr';
+import { HubConnectionBuilder } from '@microsoft/signalr/dist/esm/HubConnectionBuilder';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
   myName: string = '';
+  private chatConnection?: HubConnection;
 
   constructor(private httpClient: HttpClient) { }
 
   registerUser(user: User){
     return this.httpClient.post(`${environment.apiUrl}api/chat/register-user`,user,{responseType:'text'})
   }
+
+  createChatConnection(){
+    this.chatConnection = new HubConnectionBuilder().withUrl(`${environment.apiUrl}Hubs/chat`).withAutomaticReconnect().build();
+  }
+
 }
